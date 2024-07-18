ef7e3d04e56ea35617ddda63f2e0c7918ee0f3b6
jdale44 <placefirst@hotmail.com>
2024-07-18 10:56:25 -0400

    up

--- remote_backend_s3.tf
+++ remote_backend_s3.tf
@@ -3,6 +3,5 @@ terraform {
     bucket = "dev-proj-1-jenkins-remote-state-bucket-67584"
     key    = "terraform-jenkins/terraform.tfstate"
     region = "us-east-1"
-    key_name   = jenkins.pub
-      }
+    }
 }
