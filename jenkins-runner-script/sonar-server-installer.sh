c28aaf2797490561fb79585e2ffee279ba80c8c6
jdale44 <placefirst@hotmail.com>
2024-07-19 19:24:12 -0400

    update script

--- jenkins-runner-script/sonar-server-installer.sh
+++ jenkins-runner-script/sonar-server-installer.sh
@@ -2,3 +2,17 @@
 sudo apt-get update -y
 sleep 30
 sudo apt install openjdk-17-jdk-headless -y
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt update -y
+sudo apt-get install docker-ce docker-ce-cli docker-compose -y
+sudo service docker start
