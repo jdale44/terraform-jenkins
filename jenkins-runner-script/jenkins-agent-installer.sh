7f55fb75be3ea2f8f6ebbe44ba40a8d3edd58388
jdale44 <placefirst@hotmail.com>
2024-08-03 11:22:29 -0400

    update agaent script

--- jenkins-runner-script/jenkins-agent-installer.sh
+++ jenkins-runner-script/jenkins-agent-installer.sh
@@ -6,7 +6,7 @@ sudo apt install openjdk-17-jdk-headless -y
 sudo apt install maven -y
 sudo apt install docker.io -y
 sudo groupadd docker
-group 'docker' already exists
+sudo chmod 666 /var/run/docker.sock
 sudo usermod -aG docker $USER
 newgrp docker
 #install trivy
